ee631f6d626ff631175346080b8070d43859b7a6
Ihsan Berahim <ihsanberahim@gmail.com>
2019-07-27 08:22:45 +0800

    minor updates

--- gd-wordpress-supports.php
+++ gd-wordpress-supports.php
@@ -7,7 +7,7 @@
  * Author URI:      https://blog.ihsanberahim.com
  * Text Domain:     gd-wordpress-supports
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0.0
  *
  * @package         Gd_Wordpress_Supports
  */
@@ -22,6 +22,9 @@ define( 'GDWPS_ACTIVE', true );
 
 use \Illuminate\Support\Str;
 
+/**
+ * Sort this plugin to priorities
+ */
 add_filter( 'pre_update_option_active_plugins', function ( $plugins ) {
 	return collect( $plugins )->sortBy( function ( $plugin, $key ) use ( $plugins ) {
 		return Str::is( 'gd-wordpress-supports*', $plugin ) ? 0 : count( $plugins ) - $key;
@@ -34,12 +37,14 @@ function gdwps_activation_hook() {
 
 register_activation_hook( __FILE__, 'gdwps_activation_hook' );
 
-add_action('admin_init', function() {
-	$updateChecker = Puc_v4_Factory::buildUpdateChecker(
-		'https://github.com/ihsanberahim/gd-wordpress-supports',
-		__FILE__,
-		'gd-wordpress-supports'
-	);
 
-	$updateChecker->setBranch('master');
-});
+/**
+ * Enable version checker
+ */
+$updateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/ihsanberahim/gd-wordpress-supports',
+	__FILE__,
+	'gd-wordpress-supports'
+);
+
+$updateChecker->setBranch('master');
--- readme.txt
+++ readme.txt
@@ -5,3 +5,20 @@ Tested up to: 5.2.2
 Stable tag: 1.0.0
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Dependencies for all most plugins made by GoalDriven
+
+== Description ==
+
+this plugin bring in modern PHP framework features inspired by Laravel
+
+== Frequently Asked Questions ==
+
+= Does it dangeroes? =
+
+Nope.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release
