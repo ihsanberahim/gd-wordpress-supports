b896303e4efece93045b2cb138650a8f86fa26ba
Ihsan Berahim <ihsanberahim@gmail.com>
2019-07-30 20:20:31 +0800

    bump version

--- gd-wordpress-supports.php
+++ gd-wordpress-supports.php
@@ -7,7 +7,7 @@
  * Author URI:      https://blog.ihsanberahim.com
  * Text Domain:     gd-wordpress-supports
  * Domain Path:     /languages
- * Version:         1.1.4
+ * Version:         1.2.0
  *
  * @package         Gd_Wordpress_Supports
  */
